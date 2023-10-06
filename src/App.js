69323c6a3a2b14129846f4c64fca57a065651002
Neha Patil <nehapatil317@gmail.com>
2023-10-06 12:58:42 +0530

    Creation of payment page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Payment from './Payment';
 import Login from './Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
@@ -37,6 +38,8 @@ function App() {
           <Route path='/login' element={<Login />}>
           </Route>
           <Route path='/checkout' element={<><Header /><Checkout /></>}>
+          </Route>
+          <Route path='/payment' element={<><Header /><Payment /></>}>
           </Route>
           <Route path='/' element={<><Header /><Home /></>}>
           </Route>
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,9 +3,11 @@ import './Subtotal.css'
 import CurrencyFormat from "react-currency-format"
 import { useStateValue } from './StateProvider'
 import { getBasketTotal} from './reducer';
+import { useNavigate } from "react-router-dom";
 
 function Subtotal() {
   const [{basket}, dispatch] = useStateValue()
+  const navigate = useNavigate()
 
   return (
     <div className='subtotal'>
@@ -25,7 +27,7 @@ function Subtotal() {
         displayType={'text'}
         thousandSeparator={true} prefix={'$'}
       ></CurrencyFormat>
-      <button>Proceed to Checkout</button>
+      <button onClick={e => navigate('/payment')}>Proceed to Checkout</button>
     </div>
   )
 }
